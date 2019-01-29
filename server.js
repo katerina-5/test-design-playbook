b3935c50c7a37eda196b4b56f4a56bf2a2c2364d
polishchuk <thevivalley@gmail.com>
2019-01-29 16:21:15 +0200

    export static frontend files

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 8000;
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const path = require('path');
 
 console.log('[Server] Application start...');
 
@@ -22,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // filters
 
 app.use(cors());
-// app.use(express.static(__dirname + '/../src/assets'));
+app.use(express.static(path.resolve(__dirname + '/dist/test-design-playbook/')));
 
 app.use('/api/*', require('./api/middleware/auth_controll_filter'));
 
@@ -33,6 +34,10 @@ app.use('/api', require('./api/controllers/auth_controller'));
 app.use('/api', require('./api/controllers/result_controller'));
 app.use('/api', require('./api/controllers/model_controller'));
 app.use('/api', require('./api/controllers/user_controller'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname + '/dist/test-design-playbook/index.html'));
+});
 
 // start
 app.listen(PORT, () => {
