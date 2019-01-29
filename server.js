0117dfca0e9f12b914f2b1c58a1bb9ead3325141
polishchuk <thevivalley@gmail.com>
2019-01-29 15:36:26 +0200

    setup docker

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const cors = require('cors');
 console.log('[Server] Application start...');
 
 // connect to database
-require('./data_source/mongodb_connect')();
+require('./api/data_source/mongodb_connect')();
 
 // support json encoded bodies
 const bodyParser = require('body-parser');
@@ -24,15 +24,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 // app.use(express.static(__dirname + '/../src/assets'));
 
-app.use('/api/*', require('./middleware/auth_controll_filter'));
+app.use('/api/*', require('./api/middleware/auth_controll_filter'));
 
 console.log('[Server] filters load');
 
 // routes
-app.use('/api', require('./controllers/auth_controller'));
-app.use('/api', require('./controllers/result_controller'));
-app.use('/api', require('./controllers/model_controller'));
-app.use('/api', require('./controllers/user_controller'));
+app.use('/api', require('./api/controllers/auth_controller'));
+app.use('/api', require('./api/controllers/result_controller'));
+app.use('/api', require('./api/controllers/model_controller'));
+app.use('/api', require('./api/controllers/user_controller'));
 
 // start
 app.listen(PORT, () => {
